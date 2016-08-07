567450543760454e0fa42ea4e1126f3d29a70c4e
Papp Oliver <pappolivr@gmail.com>
2016-08-07 22:55:04 +0200

    c1

--- assets/js/console.js
+++ assets/js/console.js
@@ -8,14 +8,22 @@
           setTimeout(function() {
                var n = str.length;
 
-               var consoletext = document.getElementById(lineID);
-               consoletext.innerHTML = str+'<span class="cursor" id='+consoleID+'>&nbsp;</span>';
-               var element = document.getElementById(consoleID);
-               element.style.setProperty("-webkit-transition", "typing "+duration+"s steps("+n+", end), blinking 1s step-end infinite");
-               element.style.setProperty("animation", "typing "+duration+"s steps("+n+", end), blinking 1s step-end infinite");  
+               var element = document.getElementById(lineID)
+               element.innerHTML = "";
+               var spans = '<span>' + str.split('').join('</span><span>') + '<span class="blinking-cursor"></span></span>';
+               $(spans).hide().appendTo('.css-typing').each(function (i) {
+                   $(this).delay(100 * i).css({
+                       display: 'inline',
+                       opacity: 0
+                   }).animate({
+                       opacity: 1
+                   }, 100);
+               });
+               
           }, delay);
      }
      
-     writeConsole(5, "azebredes.github.io sadsadasdsads", "consoletext1", "consoleID1", 0);
-     //writeConsole(5, "github.io sadsadasdsads", "consoletext1", "consoleID1", 5 * 1000);
-     //writeConsole(5, "Almafa2 almafa1", "consoletext1", "consoleID1", 5 * 2000);
+
+     writeConsole(5, "azebredes.github.io sadsadasdsads Almafa2 almafa1 Almafa2 almafa1", "consoletext1", "consoleID1", 0);
+     //writeConsole(5, "github.io sadsadasdsads Almafa2 almafa1 Almafa2 almafa1", "consoletext1", "consoleID1", 5 * 1000);
+     //writeConsole(5, "Almafa2 almafa1 Almafa2 almafa1 Almafa2 almafa1 Almafa2 almafa1", "consoletext1", "consoleID1", 5 * 2000);
